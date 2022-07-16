fe0af4c3ca5493a9ca0b1c858d8405ace7a0993d
Matthias Oberholzer <moberholzer.mmt-b2020@fh-salzburg.ac.at>
2022-07-16 17:11:36 +0200

    added catch block

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,18 +3,22 @@
   import Marks from '../marks.svelte'
 
   const width = 960
-  const height = 500
+  const height = 960
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-{#await getTopoData()}
-  <p>loading map</p>
-{:then data}
-  <svg {width} {height}>
-    <Marks {data} />
-  </svg>
-{/await}
+<main>
+  {#await getTopoData()}
+    <p>loading map</p>
+  {:then data}
+    <svg {width} {height}>
+      <Marks {data} />
+    </svg>
+  {:catch error}
+    <p>error while loading map</p>
+  {/await}
+</main>
 
 <style global>
   body {
