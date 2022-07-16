3682ecbc5f39180b47771b12cae571c66984a030
Matthias Oberholzer <moberholzer.mmt-b2020@fh-salzburg.ac.at>
2022-07-16 18:29:28 +0200

    added mousover

---
+++ src/land.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let path
+
+  let active = false
+
+  function handleMouseEnter(event) {
+    active = true
+  }
+  function handleMouseLeave(event) {
+    active = false
+  }
+</script>
+
+<path
+  class={`land ${active && 'active'}`}
+  d={path}
+  on:mouseenter={handleMouseEnter}
+  on:mouseleave={handleMouseLeave}
+/>
+
+<style>
+  .land {
+    fill: #010101;
+    stroke: red;
+    cursor: pointer;
+  }
+  .active {
+    fill: blue;
+  }
+</style>
